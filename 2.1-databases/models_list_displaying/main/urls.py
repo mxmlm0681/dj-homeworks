a4dd56dff3c073611b3af1b5f9c86cef2f71b433
maksimmarchenko <max-81mail.ru@mail.ru>
2024-05-28 04:45:50 +0300

    initial commit

--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, books_list, books_list_in_date
 
 urlpatterns = [
     path('', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/', books_list, name='books_list'),
+    path('books/<pub_date>/', books_list_in_date, name='books_list')
 ]
