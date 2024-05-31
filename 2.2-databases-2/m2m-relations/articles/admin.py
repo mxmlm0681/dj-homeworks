73f037c623a11b6def5716196b6aae98479bbf91
maksimmarchenko <max-81mail.ru@mail.ru>
2024-05-31 05:31:26 +0300

    initial commit

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Relationship, Tag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        counter = 0
+        for form in self.forms:
+            for key, value in form.cleaned_data.items():
+                if key == 'is_main' and value is True:
+                    counter += 1
+        if counter > 1:
+            raise ValidationError('Основной раздел может быть только один.')
+        if counter == 0:
+            raise ValidationError('Укажите основной раздел.')
+
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Relationship
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+
+    @admin.register(Tag)
+    class TagAdmin(admin.ModelAdmin):
+        pass
