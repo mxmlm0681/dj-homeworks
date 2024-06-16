85d60eeb0726576862f9daaa423dfcd77936d8e9
maksimmarchenko <max-81mail.ru@mail.ru>
2024-06-16 07:16:12 +0300

    initial commit

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from measurement.views import SensorView, DetailView, MeasurementsView
+
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorView.as_view()),
+    path('sensors/<pk>/', DetailView.as_view()),
+    path('measurements/', MeasurementsView.as_view()),
 ]
